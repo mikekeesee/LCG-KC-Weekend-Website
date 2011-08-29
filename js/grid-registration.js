f74d20266dc56534c99fd10e0b7ad8967ab98ffd
Michael Keesee <mkeesee@gmail.com>
2011-08-29 07:59:18 -0500

    Removed and added grid files.

--- js/grid-registration.js
+++ js/grid-registration.js
@@ -1,17 +1,3 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>KC Weekend Registration</title>
-
-<link rel="stylesheet" type="text/css" media="screen" href="js/css/start/jquery-ui-1.7.2.custom.css" />
-<link rel="stylesheet" type="text/css" media="screen" href="js/css/ui.jqgrid.css" />
-
-<script type="text/javascript" src="js/jquery-1.3.2.min.js"></script>
-<script src="js/i18n/grid.locale-en.js" type="text/javascript"></script>
-<script type="text/javascript" src="js/jquery.jqGrid.min.js"></script>
-
-<script type="text/javascript">
 jQuery(document).ready(function() {
 	jQuery("#reg-registration").jqGrid({
 		url:'db-registration.php',
@@ -38,14 +24,3 @@ jQuery(document).ready(function() {
 		caption:"Registration Information"
 	}).navGrid('#reg-registration',{edit:false,add:false,del:false});
 });
-</script>
-
-</head>
-
-<body style="background-color:#5f92d2">
-
-	<table id="reg-registration"></table>
-	<div id="reg-registration-pager"></div>
-
-</body>
-</html>
